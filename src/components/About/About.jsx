2c116f009e4e08d38beaaf9d808ab5068a56ba4c
Sidd-hass <siddhantpandey203@gmail.com>
2025-12-04 20:32:16 +0530

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/16YIiEt3OsCbvjw2KJDDJoTqUGj-HU59R/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1KWB24Jt5SgBVXr9QoG-bNEu7mLVca9Zf/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
